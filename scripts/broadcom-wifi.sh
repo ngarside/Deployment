888f09ae795c9bf2b8ac7144f9d1d70acb024974
Nathan Garside <ngarside@users.noreply.github.com>
2021-01-19 11:03:00 +0000

    Update broadcom-wifi.sh

--- scripts/broadcom-wifi.sh
+++ scripts/broadcom-wifi.sh
@@ -1,4 +1,7 @@
 # ------------------------------------------------------------------------------
+# Broadcom wireless adaptor setup
+# ------------------------------------------------------------------------------
+
 # Configures wireless adaptor
 # openSUSE only
 # Usage:
@@ -6,15 +9,21 @@
 # https://en.opensuse.org/Additional_package_repositories
 # https://pmbs.links2linux.de/project/show/Essentials
 # If running script locally must be run as root
+
+# ------------------------------------------------------------------------------
+# Adds the packman essentials repository if it hasn't already been added
 # ------------------------------------------------------------------------------
 
 has_repo=$(zypper lr --details | grep https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials)
-
 if [[ -z "$has_repo" ]]
 then
 	zypper ar -cfp 90 https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials packman-essentials
 	zypper dup --from packman-essentials --allow-vendor-change
 	zypper refresh
 fi
+
+# ------------------------------------------------------------------------------
+# Installs the broadcom driver
+# ------------------------------------------------------------------------------
 
 zypper install broadcom-wl
