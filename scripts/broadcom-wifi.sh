6500f3f66e1b2458009696d44cff2b04eec1cc50
Nathan Garside <ngarside@users.noreply.github.com>
2021-01-19 10:47:29 +0000

    Update broadcom-wifi.sh

--- scripts/broadcom-wifi.sh
+++ scripts/broadcom-wifi.sh
@@ -1,6 +1,9 @@
 # ------------------------------------------------------------------------------
 # Configures wireless adaptor
 # openSUSE only
+# Run with:
+# https://en.opensuse.org/Additional_package_repositories
+# https://pmbs.links2linux.de/project/show/Essentials
 # ------------------------------------------------------------------------------
 
 sudo zypper ar -cfp 90 https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials packman-essentials
