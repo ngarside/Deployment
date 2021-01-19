d06db86238eb1331f22769c758495998ed260d70
Nathan Garside <ngarside@users.noreply.github.com>
2021-01-19 10:52:43 +0000

    Update broadcom-wifi.sh

--- scripts/broadcom-wifi.sh
+++ scripts/broadcom-wifi.sh
@@ -10,7 +10,7 @@
 
 zypper ar -cfp 90 https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials packman-essentials
 
-zypper dup --from packman --allow-vendor-change
+zypper dup --from packman-essentials --allow-vendor-change
 
 zypper refresh
 
