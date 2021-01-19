2ed76514f11cf291c0c2c49ffbbd949a665e417d
Nathan Garside <ngarside@users.noreply.github.com>
2021-01-19 10:56:29 +0000

    Update broadcom-wifi.sh

--- scripts/broadcom-wifi.sh
+++ scripts/broadcom-wifi.sh
@@ -8,10 +8,13 @@
 # If running script locally must be run as root
 # ------------------------------------------------------------------------------
 
-zypper ar -cfp 90 https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials packman-essentials
+has_repo=$(zypper lr --details | grep https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials)
 
-zypper dup --from packman-essentials --allow-vendor-change
-
-zypper refresh
+if [[ -z "$has_repo" ]]
+then
+	zypper ar -cfp 90 https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials packman-essentials
+	zypper dup --from packman-essentials --allow-vendor-change
+	zypper refresh
+fi
 
 zypper install broadcom-wl
