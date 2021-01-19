33940bf3beab1473c6499760bac329d846a81d8f
Nathan Garside <ngarside@users.noreply.github.com>
2021-01-19 11:04:57 +0000

    Update broadcom-wifi.sh

--- scripts/broadcom-wifi.sh
+++ scripts/broadcom-wifi.sh
@@ -17,8 +17,7 @@
 has_repo=$(zypper lr --details | grep https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials)
 if [[ -z "$has_repo" ]]
 then
-	zypper ar -cfp 90 https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials packman-essentials
-	zypper dup --from packman-essentials --allow-vendor-change
+	zypper addrepo --check https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials packman-essentials
 	zypper refresh
 fi
 
