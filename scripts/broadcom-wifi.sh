d66121c5119619140b88c227272b2292c739cc8d
Nathan Garside <code@nathangarside.com>
2021-01-24 13:14:25 +0000

    Update broadcom-wifi.sh

--- scripts/broadcom-wifi.sh
+++ scripts/broadcom-wifi.sh
@@ -17,7 +17,7 @@
 has_repo=$(zypper lr --details | grep https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials)
 if [[ -z "$has_repo" ]]
 then
-	zypper addrepo --check https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials packman-essentials
+	zypper addrepo --check --no-gpgcheck https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials packman-essentials
 	zypper refresh
 fi
 
@@ -25,4 +25,4 @@ fi
 # Installs the broadcom driver
 # ------------------------------------------------------------------------------
 
-zypper install broadcom-wl
+zypper --non-interactive --no-cd install --no-recommends broadcom-wl
